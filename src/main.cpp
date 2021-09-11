641e7fb93cf7fd2b73270154305e56ab7c20bbec
siwangqishiq <525647740@qq.com>
2021-09-11 12:38:44 +0800

    create instance

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#include <string>
+#include <vector>
+
 /**
  * main
  * */
@@ -19,6 +22,8 @@ const uint32_t HEIGHT = 800;
 
 class HelloTriangleApplication{
 public: 
+    std::string appName = "Hi! Vulkan";
+
     int run(){
         initWindow();
 
@@ -31,17 +36,62 @@ public:
 private:
     GLFWwindow *window = nullptr;
 
+    VkInstance instance;
+
     void initWindow(){
         glfwInit();
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "你好Vulkan", nullptr, nullptr);
+        window = glfwCreateWindow(WIDTH, HEIGHT, appName.c_str(), nullptr, nullptr);
     }
 
     void initVulkan(){
+        createInstance();
+    }
 
+    void createInstance(){
+        std::cout << "create vulkan instance " << std::endl;
+
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "HelloTriangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "NoEngine";
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0 , 0);
+
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        std::cout << "glfw extension :" << std::endl;
+        for(int i = 0 ; i < glfwExtensionCount ; i++){
+            std::cout << '\t' << glfwExtensions[i] << std::endl;
+        }//end for i
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr , &extensionCount , nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr , &extensionCount , extensions.data());
+        std::cout << "available extensions:\n";
+        for(const auto &ex : extensions){
+            std::cout << '\t' << ex.extensionName << '\t' << ex.specVersion << std::endl;
+        }//end for each
     }
 
     void mainloop(){
@@ -51,6 +101,8 @@ private:
     }
 
     void cleanup(){
+        vkDestroyInstance(instance , nullptr);
+
         glfwDestroyWindow(window);
         glfwTerminate();
     }
